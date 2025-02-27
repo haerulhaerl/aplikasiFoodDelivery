1862fbd2037c6df8aeee749248131bcc65a6dcfb
Haerul <haerulhaerlhaerl2gmail.com>
2025-02-27 23:09:00 +0800

    FoodDelivery

---
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+val Pink = Color (0xFFF28482)
+val Green = Color (0xFF84A59D)
+val Yellow = Color (0xFFF7EDE2)
+val YellowLight = Color (0xFFFFFFF2)
+val Dark  = Color (0xFF3D405B)
+
+
+@Immutable
+data class AppColors(
+    val background: Color
+)
