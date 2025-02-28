bce28c93309a882f9aacac3d4c50c0d3300bc2dd
Haerul <haerulhaerlhaerl2gmail.com>
2025-02-28 23:04:49 +0800

    Update warna tema di Color.kt

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication.ui.theme
 
+import android.view.Surface
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val Pink = Color (0xFFF28482)
@@ -12,5 +15,33 @@ val Dark  = Color (0xFF3D405B)
 
 @Immutable
 data class AppColors(
-    val background: Color
+    val background: Color,
+    val  onBackground : Color,
+    val surface: Color,
+    val onSurface:Color,
+    val secondarySurface:Color,
+    val onSecondarySurface: Color,
+    val regularSurvace:Color,
+    val onRegularSurface:Color,
+    val actionSurface:Color,
+    val onActionSurface:Color,
+    val highlightSurface: Color,
+    val onHighlightSurface: Color
 )
+
+val localAppcolors = staticCompositionLocalOf {
+    AppColors(
+        background = Color.Unspecified,
+        onBackground = Color.Unspecified,
+        surface = Color.Unspecified,
+        onSurface = Color.Unspecified,
+        secondarySurface = Color.Unspecified,
+        onSecondarySurface = Color.Unspecified,
+        regularSurvace = Color.Unspecified,
+        onRegularSurface = Color.Unspecified,
+        actionSurface = Color.Unspecified,
+        onActionSurface = Color.Unspecified,
+        highlightSurface = Color.Unspecified,
+        onHighlightSurface = Color.Unspecified
+    )
+}
