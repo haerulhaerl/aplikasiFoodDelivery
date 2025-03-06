e75b70b9e92c212f7edb652d79e4b53cc3e912a2
Haerul <haerulhaerlhaerl2gmail.com>
2025-03-06 19:07:16 +0800

    perubahan depededency

--- app/src/main/java/com/example/myapplication/ui/theme/screen/components/ProductPreviewSection.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/screen/components/ProductPreviewSection.kt
@@ -1,10 +1,20 @@
 package com.example.myapplication.ui.theme.screen.components
 
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ProductPreviewSection(modifier: Modifier = Modifier
 ) {
+    val scrollableState = rememberScrollState()
 
+    Column (
+        modifier = modifier.verticalScroll(scrollableState)
+    ){
+    }
 }
